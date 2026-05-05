de27cb69bcf6b8ce6ec17bc6d19b4bb3f3cc691d
RaghavNagi <raghavnagi81@gmail.com>
2026-05-05 20:16:11 +0530

    user and product model

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require('express')
+const app = express()
 
-app = express()
+const cookieParser = require('cookie-parser')
+const path = require('Path')
 
-app.json()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "public")))
+app.set("view engine", "ejs")
+
+
+app.get("/", (req,res) => {
+    res.send("hey")
+})
+
+
+
+app.listen(3000)
